28e9aacf695784e934b1d26d1ceab6c957b8ba9c
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-02 22:25:35 -0800

    commit tree f789d6c0a6613ba8cd9b31753ca17ba057fa6171

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,29 @@
 Actor.destroy_all
 Character.destroy_all
-Shows.destroy_all
+Show.destroy_all
 
-30.times do 
+30.times { 
     Actor.create(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name
     )
+}
 
-15.times do
-    Character.create(
-        name: Faker::FunnyName.name,
-        actor_id: rand(1..30)
-        show_id: rand
-    )
-
-20.times do
-    Shows.create(
+20.times {
+    Show.create(
         name: Faker::Movie.title,
         day: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].sample,
-        season: ["Winter", "Spring", "Summer", "Fall"].sample
+        season: ["Winter", "Spring", "Summer", "Fall"].sample,
         genre: Faker::Book.genre
     )
+}
 
-end
+15.times {
+    Character.create(
+        name: Faker::FunnyName.name,
+        actor_id: Actor.all.map {|a| a.id}.sample,
+        show_id: Show.all.map {|s| s.id}.sample,
+        catchphrase: Faker::ChuckNorris.fact
+    )
+}
 
-# data for shows
-    # name, day, season, genre, all strings
