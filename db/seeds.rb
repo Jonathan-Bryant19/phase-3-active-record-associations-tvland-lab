0733f5e709af7cbbaceb07c14fb7fd6838f86e2a
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-02 19:42:13 -0800

    Back to desktop...

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,29 @@
+Actor.destroy_all
+Character.destroy_all
+Shows.destroy_all
+
 30.times do 
     Actor.create(
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name
     )
 
-10.times do
+15.times do
     Character.create(
         name: Faker::FunnyName.name,
-        
+        actor_id: rand(1..30)
+        show_id: rand
     )
 
+20.times do
+    Shows.create(
+        name: Faker::Movie.title,
+        day: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"].sample,
+        season: ["Winter", "Spring", "Summer", "Fall"].sample
+        genre: Faker::Book.genre
+    )
 
 end
-
-# data for characters
-    # name (string), actor_id (integer), show_id (integer)
 
 # data for shows
     # name, day, season, genre, all strings
